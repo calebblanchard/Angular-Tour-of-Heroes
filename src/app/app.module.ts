f286b78c360b1555f388dc421f90eb8c6d81b814
calebblanchard <caleb.blanchard@maine.edu>
2017-09-20 10:20:38 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+
+  declarations: [
+    AppComponent
+  ],
+  
+  imports: [
+    BrowserModule,
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule
+  ],
+  
+  providers: [],
+  
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
